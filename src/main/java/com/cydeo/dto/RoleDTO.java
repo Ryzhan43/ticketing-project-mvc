986ee9f083177d02f9fa26e5a612b48f3551e648
meekkeek <ryzhan43@gmail.com>
2023-03-19 18:40:56 -0400

    upd

--- src/main/java/com/cydeo/dto/RoleDTO.java
+++ src/main/java/com/cydeo/dto/RoleDTO.java
@@ -1,5 +1,12 @@
 package com.cydeo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class RoleDTO {
 
     private Long id;
--- src/main/java/com/cydeo/dto/UserDTO.java
+++ src/main/java/com/cydeo/dto/UserDTO.java
@@ -2,12 +2,19 @@ package com.cydeo.dto;
 
 import com.cydeo.entity.Role;
 import com.cydeo.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
 public class UserDTO {
     private String firstName;
     private String lastName;
     private String userName;
     private String passWord;
+    private String email;
     private boolean enabled;
     private String phone;
     private RoleDTO roleDTO;
