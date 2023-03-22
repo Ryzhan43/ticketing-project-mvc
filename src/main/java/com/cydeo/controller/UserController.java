21b6943d322a52eb5e8e5f8de33075033bb67b2a
meekkeek <ryzhan43@gmail.com>
2023-03-21 21:06:33 -0400

    Added post method

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -5,9 +5,12 @@ import com.cydeo.dto.RoleDTO;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.service.RoleService;
 import com.cydeo.service.UserService;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -25,15 +28,24 @@ public class UserController {
     @GetMapping("/create")
     public String createUser(Model model) {
 
-
         model.addAttribute("user", new UserDTO());
         model.addAttribute("roles", roleService.FindAll());
         model.addAttribute("employees", userService.FindAll());
 
-
-
         return "user/create";
 
+    }
+
+    @PostMapping("/create")
+    public String insertUser(@ModelAttribute("") UserDTO userDTO, Model model){
+        model.addAttribute("user", new UserDTO());
+        model.addAttribute("roles", roleService.FindAll());
+
+        model.addAttribute(userDTO);
+
+        model.addAttribute("employees", userService.FindAll());
+
+        return "redirect:user/create";
     }
 
 }
